1f6f66a8d97bf9ace84c8bf21e5ca975b97a6ef1
jon-cron <jonathanacm95@gmail.com>
2023-05-01 10:37:44 -0600

    using react navigator

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 // NOTE first run 'npm install @react-navigation/native'
 // NOTE then run 'expo install react-native-screens react-native-safe-area-context' IF YOU ARE USING EXPO
+// NOTE then run 'expo install @react-navigation/native-stack'
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen.js";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       {/* NOTE changing the statusbar to light changes the icons like wifi and battery to white instead of dark */}
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <CategoriesScreen />
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
