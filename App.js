beca5d6a69bfd3c2bee7bb02b8950c8a6d6b6a0f
jon-cron <jonathanacm95@gmail.com>
2023-05-01 10:31:03 -0600

    push navigation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+// NOTE first run 'npm install @react-navigation/native'
+// NOTE then run 'expo install react-native-screens react-native-safe-area-context' IF YOU ARE USING EXPO
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen.js";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <>
       {/* NOTE changing the statusbar to light changes the icons like wifi and battery to white instead of dark */}
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
   );
 }
